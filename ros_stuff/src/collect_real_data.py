4e29f54053ec82036f526fdfa56c6fb30b380d4f
Brandon <estephan@berkeley.edu>
2022-05-13 02:54:48 -0700

    new ros stuff

--- ros_stuff/src/collect_real_data.py
+++ ros_stuff/src/collect_real_data.py
@@ -8,13 +8,14 @@ from tf.transformations import euler_from_quaternion
 
 import rospy
 
-AVG_STEPS = 5
+AVG_STEPS = 2
 
 class DataCollector:
     def __init__(self):
         print("started")
-        self.min_action = -0.7
-        self.max_action = 0.7
+        action = 0.7
+        self.min_action = -action
+        self.max_action = action
         self.current_state = np.zeros(3)        # (x, y, theta)
         self.base_state = np.zeros(3)
         self.robot_id = 0
@@ -22,10 +23,12 @@ class DataCollector:
         self.stamped_states = []
         self.stamped_actions = []
         rospy.init_node("data_collector")
+        self.time = rospy.get_rostime().to_sec()
         rospy.wait_for_service('/kami1/server')
         self.command_action = rospy.ServiceProxy('/kami1/server', CommandAction)
-        rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.collect_data, queue_size=10)
+        rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.collect_data, queue_size=1)
         rospy.spin()
+        np.savez_compressed("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data_raw.npz", states=self.stamped_states, actions=self.stamped_actions)
 
     def collect_data(self, msg):
         try:
@@ -44,32 +47,37 @@ class DataCollector:
                 o = marker.pose.pose.orientation
                 o_list = [o.x, o.y, o.z, o.w]
                 _, _, z = euler_from_quaternion(o_list)
-                state[2] = z % (2 * np.pi) * 180 / np.pi    
+                state[2] = z % (2 * np.pi)
         except:
             print("could not update states")
             import pdb;pdb.set_trace()
         
         current_state = self.current_state - self.base_state
         current_state[2] %= 2 * np.pi
-        time_state = np.append(rospy.get_rostime().to_sec(), self.current_state)
+        time_state = np.append(rospy.get_rostime().to_sec(), current_state)
         self.stamped_states.append(time_state)
         action = np.random.uniform(low=self.min_action, high=self.max_action, size=2)
-        print(action)
+        if rospy.get_rostime().to_sec() - self.time > 0.5:
+            self.time = rospy.get_rostime().to_sec()
+            print(f"action: {action}")
+            print(f"base: {self.base_state}")
+            print(f"state:, {current_state}")
         action_req = RobotCmd()
         action_req.left_pwm = action[0]
         action_req.right_pwm = action[1]
         timestamp = self.command_action(action_req, 'kami1')
-        print("sent action")
-        time_action = np.append(timestamp.time.to_sec(), action)
-        # time_action = np.append(1000, action)
+        time_action = np.append(timestamp.time, action)
         self.stamped_actions.append(time_action)
+        rospy.sleep(0.3)
     
     def process_raw_data(self):
-        state_times = self.stamped_states[:, 0]
-        action_times = self.stamped_actions[:, 0]
+        stamped_states = np.array(self.stamped_states)
+        stamped_actions = np.array(self.stamped_actions)
+        state_times = stamped_states[:, 0]
+        action_times = stamped_actions[:, 0]
 
-        states = self.stamped_states[:, 1:]
-        actions = self.stamped_actions[:, 1:]
+        states = stamped_states[:, 1:]
+        actions = stamped_actions[:, 1:]
 
         all_states = []
         all_actions = []
@@ -80,17 +88,28 @@ class DataCollector:
             idx = np.where(state_times < t_action)[0].max()
             if idx < AVG_STEPS - 1 or len(states) - idx < AVG_STEPS + 1:
                 continue
-            current_state = states[idx-AVG_STEPS+1:idx+1].mean()
-            next_state = states[idx+1:idx+1+AVG_STEPS].mean()
+            current_state = states[idx-AVG_STEPS+1:idx+1].mean(axis=0)
+            next_state = states[idx+1:idx+1+AVG_STEPS].mean(axis=0)
             all_states.append(current_state)
             all_actions.append(action)
             print("CUR STATE: ", current_state)
             all_next_states.append(next_state)
         
-        np.savez_compressed("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data.npz",
-                            states=all_states, actions=all_actions, next_states=all_next_states)
+        np.savez_compressed("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data.npz", states=all_states, actions=all_actions, next_states=all_next_states)
 
-            
+def process(stamped_states, stamped_actions):
+    stamped_states = np.array(stamped_states)
+    stamped_actions = np.array(stamped_actions)
+    all_states = stamped_states[:-1, 1:]
+    all_actions = stamped_actions[:-1, 1:]
+    all_next_states = stamped_states[1:, 1:]
+    length = min(len(all_states), len(all_actions))
+    all_states = all_states[:length]
+    all_actions = all_actions[:length]
+    all_next_states = all_next_states[:length]
+    
+    np.savez_compressed("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data_one.npz", states=all_states, actions=all_actions, next_states=all_next_states)
+
 if __name__ == '__main__':
     dc = DataCollector()
     
@@ -100,3 +119,8 @@ if __name__ == '__main__':
     except:
         print("could not process raw data")
         import pdb;pdb.set_trace()
+    
+    data = np.load("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data_raw.npz")
+    stamped_states = data['states']
+    stamped_actions = data['actions']
+    process(stamped_states, stamped_actions)
--- ros_stuff/src/new_serv_test.py
+++ ros_stuff/src/new_serv_test.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 from ros_stuff.msg import RobotCmd
-import numpy as np
 import rospy
-from ros_stuff.srv import CommandAction  # Service type
+from ros_stuff.srv import CommandAction, CommandActionResponse  # Service type
 import sys
 from std_msgs.msg import String
 import math
@@ -19,25 +18,26 @@ MOTOR_LEFT_PWM = 13 # PWMA - 33, GPIO 13
 MOTOR_LEFT_FW = 22 # AIN2 - 15, GPIO 22
 MOTOR_LEFT_BW = 27 # AIN1 - 13, GPIO 27
 
-
 def kami_callback(request):
-    motor_standby = DigitalOutputDevice(MOTOR_STANDBY)
-    motor_left_pwm = PWMOutputDevice(MOTOR_LEFT_PWM)
-    motor_left_forward = DigitalOutputDevice(MOTOR_LEFT_FW)
-    motor_left_backward = DigitalOutputDevice(MOTOR_LEFT_BW)
-    motor_right_pwm = PWMOutputDevice(MOTOR_RIGHT_PWM)
-    motor_right_forward = DigitalOutputDevice(MOTOR_RIGHT_FW)
-    motor_right_backward = DigitalOutputDevice(MOTOR_RIGHT_BW)
-    ports = [motor_standby, motor_left_pwm, motor_left_forward, motor_left_backward,
-        motor_right_pwm, motor_right_forward, motor_right_backward]
-    motor_standby.on()
+    print("Hi, I got a message")
+    print(request)
+    left_forward = request.robot_cmd.left_pwm > 0
+    right_forward = request.robot_cmd.right_pwm > 0
     
-    motor_left_forward.on()
-    motor_left_backward.off()
+    if left_forward:
+        motor_left_forward.on()
+        motor_left_backward.off()
+    else:
+        motor_left_forward.off()
+        motor_left_backward.on()
     motor_left_pwm.on()
 
-    motor_right_forward.on()
-    motor_right_backward.off()
+    if right_forward:
+        motor_right_forward.on()
+        motor_right_backward.off()
+    else:
+        motor_right_forward.off()
+        motor_right_backward.on()
     motor_right_pwm.on()
 
     motor_left_pwm.value = 0.0
@@ -52,15 +52,16 @@ def kami_callback(request):
 
     r = rospy.Rate(10)
 
-    while not rospy.is_shutdown():
+    # while not rospy.is_shutdown():
         # class_name.set_pwm(left_val, right_val)
-        motor_right_pwm.value = right_val
-        motor_left_pwm.value = left_val
-        pubstr = name + " pwm " + str(left_val) + ", " + str(right_val)
-        pub.publish(pubstr)
-        print(rospy.get_name() + ": I sent", pubstr)
-        pub.publish(pubstr)  # Publish to cmd_topic
-        r.sleep()
+    motor_right_pwm.value = abs(right_val)
+    motor_left_pwm.value = abs(left_val)
+    pubstr = name + " pwm " + str(left_val) + ", " + str(right_val)
+    # pub.publish(pubstr)
+    print(rospy.get_name() + ": I sent", pubstr)
+    # pub.publish(pubstr)  # Publish to cmd_topic
+    # r.sleep()
+    return CommandActionResponse(name, rospy.get_rostime().to_sec())
 
 def robot_server(name):
     # # Create class instance for specific kamigami
@@ -68,16 +69,30 @@ def robot_server(name):
     # specific = KamigamiNode(name)
 
     # Initialize the server node for specific kamigami
+    print("started")
     rospy.init_node('{}_robot_server'.format(name))
+    print("made node")
     # Register service
     rospy.Service(
         '/{}/server'.format(name),  # Service name
         CommandAction,  # Service type
         kami_callback  # Service callback
     )
+    print("registered service")
     rospy.loginfo('Running robot server...')
     rospy.spin() # Spin the node until Ctrl-C
 
 
 if __name__ == '__main__':
+    motor_standby = DigitalOutputDevice(MOTOR_STANDBY)
+    motor_left_pwm = PWMOutputDevice(MOTOR_LEFT_PWM)
+    motor_left_forward = DigitalOutputDevice(MOTOR_LEFT_FW)
+    motor_left_backward = DigitalOutputDevice(MOTOR_LEFT_BW)
+    motor_right_pwm = PWMOutputDevice(MOTOR_RIGHT_PWM)
+    motor_right_forward = DigitalOutputDevice(MOTOR_RIGHT_FW)
+    motor_right_backward = DigitalOutputDevice(MOTOR_RIGHT_BW)
+    ports = [motor_standby, motor_left_pwm, motor_left_forward, motor_left_backward,
+        motor_right_pwm, motor_right_forward, motor_right_backward]
+    motor_standby.on()
+    
     robot_server(sys.argv[1])
